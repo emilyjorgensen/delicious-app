179f555dc1fc6a0ef4459e2c7601e2bf1a1201da
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2024-01-31 12:27:05 -0600

    map through random recipes for Popular component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,22 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     getPopular();
   }, []);
 
   const getPopular = async () => {
-    const apiKey = import.meta.env.VITE_APP_API_KEY;
     const api = await fetch(
       `https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_APP_API_KEY}&number=9`
     );
     const data = await api.json();
-    console.log(import.meta.env);
-    console.log(apiKey);
     console.log(data);
+    setPopular(data.recipes);
   };
 
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Popular;
