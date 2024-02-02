2fecb96d054e28b7157d39c9a609facf1ca18eef
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2024-02-02 12:00:27 -0600

    Routes established for Pages

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,11 @@
 import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
 import { GiNoodles, GiChopsticks } from "react-icons/gi";
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 function Category() {
   return (
-    <div>
+    <List>
       <div>
         <FaPizzaSlice />
         <h4>Italian</h4>
@@ -21,8 +22,14 @@ function Category() {
         <GiChopsticks />
         <h4>Japanese</h4>
       </div>
-    </div>
+    </List>
   );
 }
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
 
 export default Category;
